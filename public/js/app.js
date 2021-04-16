97b64cda86a29c16d70bf1f661bc5fcab122a9be
karanAjmani8982 <karanajmani72@gmail.com>
2021-04-16 19:31:46 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit',(e) => {
     messageTwo.textContent = ''
 
     // if(location.length > 0){
-        const url = 'http://localhost:3000/weather?address=' + location 
+        const url = '/weather?address=' + location 
         fetch(url).then((response) => {
             response.json().then((data) => {
                 if(data.error){
