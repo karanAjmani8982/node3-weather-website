12b58e3798fd716e53965d7a7a1ae5c821cffed8
karanAjmani8982 <karanajmani72@gmail.com>
2021-04-16 21:12:07 +0530

    change in forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+            // console.log(body.current.humidity)
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. Humidity is ' + body.current.humidity + '.')
         }
     })
 }
